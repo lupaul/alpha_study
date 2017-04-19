9ead49e6ef6377a7e01f83f6bf062b4d3fc311ef
盧柏嘉 <lupochia@LuPaul.local>
2017-04-19 20:09:42 +0800

    fix deploy path

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :deploy_to, "/home/pochia/alpha_study"
 # append :linked_files, "config/database.yml", "config/secrets.yml"
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
-:linked_files, fetch(:linked_files, []).push('config/application.yml', 'config/application.yml')
+set :linked_files, fetch(:linked_files, []).push('config/application.yml', 'config/application.yml')
 
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
