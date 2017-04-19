bb51799ca4af13cf68b8f891563d6ed032f35744
盧柏嘉 <lupochia@LuPaul.local>
2017-04-19 11:16:32 +0800

    add view if enrollment is our partner

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,10 +1,7 @@
 class Enrollment < ApplicationRecord
   belongs_to :cooperation
 
-  def add_user
-    newuser = User.new
-    newuser.name = @enrollment.name
-    newuser.school = @enrollment.school
-    newuser.email = @enrollment.email
+  def is_partner?
+    User.where(email: email).present?
   end
 end
