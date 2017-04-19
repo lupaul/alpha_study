775d3306d636a3f6f21e4a55fdeeb90d571903da
XiawPohr <paradise121329@gmail.com>
2017-04-19 13:51:05 +0800

    migration: activity

--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,21 @@ ActiveRecord::Schema.define(version: 20170419034910) do
     t.datetime "updated_at",     null: false
     t.string   "state"
 
+  create_table "activities", force: :cascade do |t|
+    t.string   "subject"
+    t.text     "description"
+    t.datetime "datatime"
+    t.string   "location"
+    t.string   "presenter_name"
+    t.text     "presenter_info"
+    t.string   "organizer"
+    t.integer  "liked"
+    t.integer  "user_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.index ["user_id"], name: "index_activities_on_user_id"
+  end
+
   create_table "courses", force: :cascade do |t|
     t.string  "title"
     t.text    "description"
