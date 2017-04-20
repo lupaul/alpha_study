2ee254e1a799fb23a44334ac30057f59547b37d5
盧柏嘉 <lupochia@LuPaul.local>
2017-04-20 18:31:07 +0800

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'welcome#index'
+
 
   namespace :admin do
     resources :schools
@@ -42,5 +42,5 @@ Rails.application.routes.draw do
     resources :schools, :licenses, :courses, :activities
     root 'factories#index'
   end
-
+  root 'welcome#index'
 end
