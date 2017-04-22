1f43cd9759e4aef78e72ec0b90018aa2a6b76718
XiawPohr <paradise121329@gmail.com>
2017-04-22 14:55:11 +0800

    schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,8 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 20170419051832) do
+
+ActiveRecord::Schema.define(version: 20170422055318) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "subject"
