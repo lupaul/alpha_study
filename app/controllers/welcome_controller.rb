a4f7a65ff68b34b2d967434e861257515dce481d
XiawPohr <paradise121329@gmail.com>
2017-04-26 16:57:24 +0800

    index frontend

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,5 +3,10 @@ class WelcomeController < ApplicationController
     # flash[:notice] = "success"
     # flash[ :warning ] = "warning!"
     # flash[ :alert ] = "alert!!"
+    @activities = Activity.all.limit(2)
+    @experts = Expert.all.limit(2)
+    @reservations = Reservation.all.limit(2)
+    @licenses = License.all.limit(2)
+    @schools = School.all.limit(2)
   end
 end
