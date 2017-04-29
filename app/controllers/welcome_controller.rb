6f292ca2c4fcf37bfdb5e08a938db3ee1f2d42a2
XiawPohr <paradise121329@gmail.com>
2017-04-29 20:43:45 +0800

    home page done

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,10 +3,10 @@ class WelcomeController < ApplicationController
     # flash[:notice] = "success"
     # flash[ :warning ] = "warning!"
     # flash[ :alert ] = "alert!!"
-    @activities = Activity.all.limit(2)
-    @experts = Expert.all.limit(2)
+    @activities   = Activity.all.limit(2)
+    @experts      = Expert.all.limit(2)
     @reservations = Reservation.all.limit(2)
-    @licenses = License.all.limit(2)
-    @schools = School.all.limit(2)
+    @categories   = Category.all.limit(3)
+    @schools      = School.all.limit(3)
   end
 end
