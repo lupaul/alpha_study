7ede3e45ca66a11ed91a0f977a1abc7b7f55019b
盧柏嘉 <lupochia@LuPaul.local>
2017-06-02 23:05:38 +0800

    fix bug upload

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -12,7 +12,7 @@ CarrierWave.configure do |config|
     config.fog_directory  = 'justudy' # bucket name
     config.storage :fog
   else
-
-    config.storage :file
+    config.storage :fog
+    # config.storage :file
   end
 end
